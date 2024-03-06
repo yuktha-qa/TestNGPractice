0b92d1f233afc1a8f505ce1b880b0ea5d1ddeba4
rmala <rmala@192.168.2.8>
2024-03-06 17:31:53 -0600

    Second commit with compare values program

--- src/listenertestng/CompareValues.java
+++ src/listenertestng/CompareValues.java
@@ -1,12 +1,18 @@
 package listenertestng;
 
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(list.TListen.class)
 public class CompareValues {
 
 	@Test
 	public void compareInt() {
 		Assert.assertEquals(20, 20);
+	}
+	@Test
+	public void compareDecimal() {
+		Assert.assertEquals(2.5, 3.5);
 	}
 }
